b508e2fa444d3753a90202f430773ad1401d7ca4
NevercomeG <luisjavier2012@gmail.com>
2023-06-09 18:30:52 -0400

    chore: deleted from crud routes rud

--- src/atoms/emailss/router.ts
+++ src/atoms/emailss/router.ts
@@ -5,11 +5,11 @@ import { getObject } from './middleware';
 const router = Router();
 
 // Define your routes here
-router.get('/', controllers.list);
-router.get('/:id', getObject, controllers.retrieve);
+// router.get('/', controllers.list);
+// router.get('/:id', getObject, controllers.retrieve);
 
 router.post('/', controllers.create);
-router.patch('/:id', getObject, controllers.update);
-router.delete('/:id', getObject, controllers.destroy);
+// router.patch('/:id', getObject, controllers.update);
+// router.delete('/:id', getObject, controllers.destroy);
 
 export default router;
